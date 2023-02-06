0ca7e2705e961becf7ea08f7473a244c8ed63fe6
moogieon <vzlzydn5007@gmail.com>
2023-02-06 15:47:17 +0900

    req,res 테스트

--- letsStart/dist/app.js
+++ letsStart/dist/app.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app = express();
 var port = 8000;
-app.get("/", function (req, res) {
-    res.send("Hello World!");
+app.get("/test", function (req, res) {
+    res.send({ name: "Lee", age: 20, friends: ["Kim", "Park"] });
 });
 app.listen(port, function () {
-    console.log("Example app listening on port ".concat(port));
+    console.log("Example app listening on port at http://localhost:".concat(port));
 });
 //# sourceMappingURL=app.js.map
--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,11 +1,11 @@
 import * as express from "express";
 const app: express.Express = express();
-const port:number= 8000;
+const port: number = 8000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/test", (req: express.Request, res: express.Response) => {
+  res.send({name:"Lee",age:20,friends:["Kim","Park"]});
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port at http://localhost:${port}`);
 });
