b08c872c5af55419d539c4b3b074e85511ac5843
moogieon <vzlzydn5007@gmail.com>
2023-02-08 15:27:56 +0900

    라우터, 미들웨어 설정

--- letsStart/dist/app.js
+++ letsStart/dist/app.js
@@ -5,12 +5,27 @@ var app_model_1 = require("./app.model");
 var app = express();
 var port = 8000;
 var data = [1, 2, 3, 4];
+app.use(function (req, res, next) {
+    console.log(req.rawHeaders[1]);
+    console.log("this is logging middleware");
+    next();
+});
 app.get("/test", function (req, res) {
-    console.log(req);
+    console.log(req.rawHeaders);
     res.send({ cats: app_model_1.Cat });
+});
+app.get("/cats/blue", function (req, res, next) {
+    res.send({ blue: app_model_1.Cat[0] });
+});
+app.get("/cats/som", function (req, res) {
+    res.send({ som: app_model_1.Cat[1] });
 });
 app.post("/test", function (req, res) {
     res.send({ person: "Yoon" });
+});
+app.use(function (req, res, next) {
+    console.log("this is logging middleware");
+    res.send({ error: "404 not fond error" });
 });
 app.listen(port, function () {
     console.log("server in on... http://localhost:".concat(port));
--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -5,13 +5,30 @@ const port: number = 8000;
 
 const data = [1, 2, 3, 4];
 
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is logging middleware");
+  next();
+});
+
 app.get("/test", (req: express.Request, res: express.Response) => {
-  console.log(req);
+  console.log(req.rawHeaders);
   res.send({ cats: Cat });
+});
+app.get("/cats/blue", (req, res, next: express.NextFunction) => {
+  res.send({ blue: Cat[0] });
+});
+app.get("/cats/som", (req: express.Request, res: express.Response) => {
+  res.send({ som: Cat[1] });
 });
 
 app.post("/test", (req, res) => {
   res.send({ person: "Yoon" });
+});
+
+app.use((req, res, next) => {
+  console.log("this is logging middleware");
+  res.send({ error: "404 not fond error" });
 });
 
 app.listen(port, () => {
