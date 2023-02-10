24c957d8bf5de34486c17c08db87b5267daa8d4a
moogieon <vzlzydn5007@gmail.com>
2023-02-10 15:21:47 +0900

    route 분리, 모듈화

--- letsStart/dist/app.js
+++ letsStart/dist/app.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
-var app_model_1 = require("./app.model");
+var cats_route_1 = require("./cats/cats.route");
 var app = express();
 var port = 8000;
 var data = [1, 2, 3, 4];
@@ -11,60 +11,7 @@ app.use(function (req, res, next) {
     next();
 });
 app.use(express.json());
-app.get("/cats", function (req, res) {
-    try {
-        var cats = app_model_1.Cat;
-        res.status(200).send({
-            success: true,
-            data: {
-                cats: cats,
-            },
-        });
-    }
-    catch (error) {
-        res.status(400).send({
-            success: false,
-            error: error.message,
-        });
-    }
-});
-app.get("/cats/:id", function (req, res) {
-    try {
-        var params_1 = req.params;
-        var cat = app_model_1.Cat.find(function (cat) {
-            return cat.id === params_1.id;
-        });
-        res.status(200).send({
-            success: true,
-            data: {
-                cat: cat,
-            },
-        });
-    }
-    catch (error) {
-        res.status(400).send({
-            success: false,
-            error: error.message,
-        });
-    }
-});
-app.post("/cats", function (req, res) {
-    try {
-        var data_1 = req.body;
-        console.log(data_1);
-        app_model_1.Cat.push(data_1);
-        res.status(200).send({
-            success: true,
-            data: { data: data_1 },
-        });
-    }
-    catch (error) {
-        res.status(400).send({
-            success: false,
-            error: error.message,
-        });
-    }
-});
+app.use(cats_route_1.default);
 app.use(function (req, res, next) {
     console.log("this is logging middleware");
     res.send({ error: "404 not fond error" });
--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,5 +1,5 @@
 import * as express from "express";
-import { Cat, CatType } from "./app.model";
+import catRouter from "./cats/cats.route";
 const app: express.Express = express();
 const port: number = 8000;
 
@@ -14,78 +14,7 @@ app.use((req, res, next) => {
 //*JSON middleware
 app.use(express.json());
 
-// app.get("/test", (req: express.Request, res: express.Response) => {
-//   console.log(req.rawHeaders);
-//   res.send({ cats: Cat });
-// });
-// app.get("/cats/blue", (req, res, next: express.NextFunction) => {
-//   res.send({ blue: Cat[0] });
-// });
-// app.get("/cats/som", (req: express.Request, res: express.Response) => {
-//   res.send({ som: Cat[1] });
-// });
-
-// app.post("/test", (req, res) => {
-//   res.send({ person: "Yoon" });
-// });
-
-//* READ 고양이 전체 데이터 조회
-app.get("/cats", (req: express.Request, res: express.Response) => {
-  try {
-    const cats = Cat;
-    // throw new Error("db connect error");
-    res.status(200).send({
-      success: true,
-      data: {
-        cats,
-      },
-    });
-  } catch (error: any) {
-    res.status(400).send({
-      success: false,
-      error: error.message,
-    });
-  }
-});
-//* READ 특정 고양이 데이터 조회
-app.get("/cats/:id", (req: express.Request, res: express.Response) => {
-  try {
-    const params = req.params;
-    const cat = Cat.find((cat) => {
-      return cat.id === params.id;
-    });
-
-    res.status(200).send({
-      success: true,
-      data: {
-        cat,
-      },
-    });
-  } catch (error: any) {
-    res.status(400).send({
-      success: false,
-      error: error.message,
-    });
-  }
-});
-
-//* CREATE 새로운 고양이 추가
-app.post("/cats", (req, res) => {
-  try {
-    const data = req.body;
-    console.log(data);
-    Cat.push(data);
-    res.status(200).send({
-      success: true,
-      data: { data },
-    });
-  } catch (error: any) {
-    res.status(400).send({
-      success: false,
-      error: error.message,
-    });
-  }
-});
+app.use(catRouter);
 //* 404 middleware
 app.use((req, res, next) => {
   console.log("this is logging middleware");
