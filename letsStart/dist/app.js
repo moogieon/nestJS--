d3e0daabb4c9eb200e5a6fe9d2221b4532092294
moogieon <vzlzydn5007@gmail.com>
2023-02-15 15:55:21 +0900

    ex[ress 싱글톤 패턴

--- letsStart/dist/app.js
+++ letsStart/dist/app.js
@@ -4,19 +4,38 @@ var express = require("express");
 var cats_route_1 = require("./cats/cats.route");
 var app = express();
 var port = 8000;
-var data = [1, 2, 3, 4];
-app.use(function (req, res, next) {
-    console.log(req.rawHeaders[1]);
-    console.log("this is logging middleware");
-    next();
-});
-app.use(express.json());
-app.use(cats_route_1.default);
-app.use(function (req, res, next) {
-    console.log("this is logging middleware");
-    res.send({ error: "404 not fond error" });
-});
-app.listen(port, function () {
-    console.log("server in on... http://localhost:".concat(port));
-});
+var Server = (function () {
+    function Server() {
+        var app = express();
+        this.app = app;
+    }
+    Server.prototype.setRoute = function () {
+        this.app.use(cats_route_1.default);
+    };
+    Server.prototype.setMiddleware = function () {
+        this.app.use(function (req, res, next) {
+            console.log(req.rawHeaders[1]);
+            console.log("this is logging middleware");
+            next();
+        });
+        this.app.use(express.json());
+        this.setRoute();
+        this.app.use(function (req, res, next) {
+            console.log("this is logging middleware");
+            res.send({ error: "404 not fond error" });
+        });
+    };
+    Server.prototype.listen = function () {
+        this.setMiddleware();
+        this.app.listen(port, function () {
+            console.log("server in on... http://localhost:".concat(port));
+        });
+    };
+    return Server;
+}());
+function init() {
+    var server = new Server();
+    server.listen();
+}
+init();
 //# sourceMappingURL=app.js.map
--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,26 +1,44 @@
 import * as express from "express";
 import catRouter from "./cats/cats.route";
+
 const app: express.Express = express();
 const port: number = 8000;
+class Server {
+  public app: express.Application;
+  constructor() {
+    const app: express.Application = express();
+    this.app = app;
+  }
+  private setRoute() {
+    this.app.use(catRouter);
+  }
+  private setMiddleware() {
+    //* looging middleware
+    this.app.use((req, res, next) => {
+      console.log(req.rawHeaders[1]);
+      console.log("this is logging middleware");
+      next();
+    });
+    //*JSON middleware
+    this.app.use(express.json());
 
-const data = [1, 2, 3, 4];
+    this.setRoute();
+    //* 404 middleware
+    this.app.use((req, res, next) => {
+      console.log("this is logging middleware");
+      res.send({ error: "404 not fond error" });
+    });
+  }
+  public listen() {
+    this.setMiddleware();
+    this.app.listen(port, () => {
+      console.log(`server in on... http://localhost:${port}`);
+    });
+  }
+}
 
-//* looging middleware
-app.use((req, res, next) => {
-  console.log(req.rawHeaders[1]);
-  console.log("this is logging middleware");
-  next();
-});
-//*JSON middleware
-app.use(express.json());
-
-app.use(catRouter);
-//* 404 middleware
-app.use((req, res, next) => {
-  console.log("this is logging middleware");
-  res.send({ error: "404 not fond error" });
-});
-
-app.listen(port, () => {
-  console.log(`server in on... http://localhost:${port}`);
-});
+function init() {
+  const server = new Server();
+  server.listen();
+}
+init();
