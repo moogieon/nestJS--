daf770eb0c4749f1038c28a0140675d8b860469e
moogieon <vzlzydn5007@gmail.com>
2023-03-09 16:37:26 +0900

    dog 데이터 복습-2

---
+++ letsStart/src/dogs/dogs.service.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+
+export const readAlldog = (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
+
+export const readdog = (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
+
+export const creatDog = (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
