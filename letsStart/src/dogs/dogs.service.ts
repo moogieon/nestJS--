8f37d2101449bcf827f0a9d0e85a05c7de5e1fe9
moogieon <vzlzydn5007@gmail.com>
2023-03-20 13:46:52 +0900

    dog 데이터 복습-4

--- letsStart/src/dogs/dogs.service.ts
+++ letsStart/src/dogs/dogs.service.ts
@@ -20,9 +20,24 @@ export const readAlldog = (req: Request, res: Response) => {
 
 export const readdog = (req: Request, res: Response) => {
   try {
-  } catch (error) {}
-};
+    const params = req.params;
+    const dog = Dog.find((dog) => {
+      return dog.id === params.id;
+    });
 
+    res.status(200).send({
+      success: true,
+      data: {
+        dog,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+};
 export const creatDog = (req: Request, res: Response) => {
   try {
   } catch (error) {}
