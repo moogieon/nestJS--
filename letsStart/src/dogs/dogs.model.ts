be90bcab8d345d5982b79db65e84c8f54c13f731
moogieon <vzlzydn5007@gmail.com>
2023-04-11 16:49:18 +0900

    데이터 테스트

--- letsStart/src/dogs/dogs.model.ts
+++ letsStart/src/dogs/dogs.model.ts
@@ -168,5 +168,89 @@ export const Dog: CatType[] = [
     isCute: false,
 
     friends: [],
+  }, {
+    id: "weju0fj20qj",
+
+    name: "orange",
+
+    age: 2,
+
+    species: "Russian Blue",
+
+    isCute: false,
+
+    friends: [],
+  }, {
+    id: "weju0fj20qj",
+
+    name: "orange",
+
+    age: 2,
+
+    species: "Russian Blue",
+
+    isCute: false,
+
+    friends: [],
+  }, {
+    id: "weju0fj20qj",
+
+    name: "orange",
+
+    age: 2,
+
+    species: "Russian Blue",
+
+    isCute: false,
+
+    friends: [],
+  }, {
+    id: "weju0fj20qj",
+
+    name: "orange",
+
+    age: 2,
+
+    species: "Russian Blue",
+
+    isCute: false,
+
+    friends: [],
+  }, {
+    id: "weju0fj20qj",
+
+    name: "orange",
+
+    age: 2,
+
+    species: "Russian Blue",
+
+    isCute: false,
+
+    friends: [],
+  }, {
+    id: "weju0fj20qj",
+
+    name: "orange",
+
+    age: 2,
+
+    species: "Russian Blue",
+
+    isCute: false,
+
+    friends: [],
+  }, {
+    id: "weju0fj20qj",
+
+    name: "orange",
+
+    age: 2,
+
+    species: "Russian Blue",
+
+    isCute: false,
+
+    friends: [],
   },
 ];
